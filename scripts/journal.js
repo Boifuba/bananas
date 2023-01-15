f64091f5b5e6fa3ef2ee574f401075f852a7d400
Cefas Sathler <cefasheli@gmail.com>
2023-01-15 19:52:09 -0300

    Update journal.js

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,8 +1,14 @@
 export function journal(args) {
     const j = game.journal.getName(args);
     if (!j) return;
-    let jContent = j.data.content;
+
+    const allElements = j.pages.filter(e => e);
+    let content = "";
+    allElements.forEach(element => {
+        content += element.text.content + "\n";
+    });
+
     new Dialog({title: "GURPS",
-                content: j.pages.find(e => e).text.content,
-                buttons: {ok: {label: "Close"}}}).render(true, {width: 620});
+                content: content,
+                buttons: {ok: {label: "Close"}}}).render(true, {width: 800});
 }
